0d7577e81fc2d779a5a132973e12dbf7c028659f
Philippe Beck <philippe@philippebeck.net>
2019-03-06 14:59:25 +0100

    Create grunt file for webkit, min, map, header & footer

---
+++ gruntfile.js
@@ -0,0 +1,48 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        concat: {
+            options: {
+                sourceMap: true,
+                sourceMapName: 'css/map/style.css.map',
+                banner: '/* <%= pkg.name %> v<%= pkg.version %> | <%= pkg.homepage %> | <%= pkg.license %> License */\n\n',
+                footer: '\n/* Author: <%= pkg.author.name %> <<%= pkg.author.email %>>\n Updated: <%= grunt.template.today("dS mmm yyyy @ h:MM:ss TT") %> */'
+            },
+            dist: {
+                src: [
+                    'node_modules/normalize.css/normalize.css',
+                    'scss/dist/style.css'
+                ],
+                dest: 'css/style.css'
+            }
+        },
+
+        postcss: {
+            options: {
+                processors: [
+                    require('autoprefixer')({browsers: 'last 2 versions'}) // add vendor prefixes
+                ]
+            },
+            dist: {
+                src: 'css/style.css'
+            }
+        },
+
+        cssmin: {
+            target: {
+                files: [{
+                    'css/min/style.min.css': ['css/style.css']
+                }]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-postcss');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+
+    grunt.registerTask('default', ['concat', 'postcss', 'cssmin']);
+};
